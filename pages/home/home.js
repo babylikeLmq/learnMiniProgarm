d07378a0ebb43631b69838d6e488dc388b417b22
lumingqing <1335881694@qq.com>
2020-03-24 23:38:56 +0800

    微信小程序的全局配置

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,51 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name : 'lmq',
+    "age": 18,
+    msg:"My favorite sport is basketball ",
+    students:[
+      { id:'001', name: 'cxk', age:24},
+      { id: '002', name: 'curry', age: 23 },
+      { id: '003', name: 'james', age: 22 },
+      { id: '004', name: 'lmq', age: 21 }
+    ],
+    counter: 0
   },
+  handleBtnClick(){
+    // console.log('按钮发生了点击')
+    // 1.错误做法
+    // this.data.counter += 1
+    // console.log(this.data.counter += 1)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
+    // 2.this.setData()
+    this.setData({
+      // counter: 100
+      counter:this.data.counter +1
+    })
   },
+  handleBtnClick() {
+    // console.log('按钮发生了点击')
+    // 1.错误做法
+    // this.data.counter += 1
+    // console.log(this.data.counter += 1)
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
+    // 2.this.setData()
+    this.setData({
+      // counter: 100
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleSubtraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  changeMeHobby(){
+    this.setData({
+      msg: "My favorite sport is badminton "
+    })
   }
-})
+}) 
