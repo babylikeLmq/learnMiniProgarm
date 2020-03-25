92da33ce68e22a1e7c511c58c1e3b800a84a2b77
lumingqing <1335881694@qq.com>
2020-03-25 19:56:55 +0800

    局部配置

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ App({
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次） 后台存活2个小时
    */
-  onLaunch: function () {
-    console.log('生命周期回调——监听小程序初始化。')
+  onLaunch: function (options) {
+   
     //发送网络请求
-    // wx:request({
-    //   url:''
-    // })
+   
     //获取用户信息
 
-    setTimeout(function(){
-      const err = new Error()
-      throw err  
-
-    },3000)
-
+    //小程序的进入场景  小程序显示出来时
+    // console.log(options)
+    // switch(options.scene){
+    //   //根据进入的场景值可以做的很多事情
+    //   case 1001:
+    //     break;
+    //   case 1005:
+    //     break;  
+    // }
   },
 
   /**
@@ -24,21 +25,41 @@ App({
    * 小程序界面显示出来之后会执行
    */
   onShow: function (options) {
-    console.log('小程序界面显示出来之后会执行')
+  
+    console.log(options)
+    switch (options.scene) {
+      //根据进入的场景值可以做的很多事情
+      case 1001:
+        break;
+      case 1005:
+        break;
+    }
+    //获取用户信息  wx 小程序提供 的api
+    wx.getUserInfo({
+      success:function(res) {
+        console.log(res)
+      }
+    })
+
   },
 
   /**
    * 当小程序从前台进入后台，会触发 onHide  界面被隐藏时
    */
   onHide: function () {
-    console.log('小程序界被隐藏时会执行')
+  
   },
 
   /**
    * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
    */
   onError: function (msg) {
-    console.log('3秒后抛出错误!!!')
     
+    
+  },
+
+  globalData:{
+    name:"溟七",
+    age:22
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,51 +1,106 @@
 // pages/home/home.js
+//getApp获取App.js中App产生的实例对象
+const app = getApp() 
+
 Page({
-  data:{
-    name : 'lmq',
-    "age": 18,
-    msg:"My favorite sport is basketball ",
-    students:[
-      { id:'001', name: 'cxk', age:24},
-      { id: '002', name: 'curry', age: 23 },
-      { id: '003', name: 'james', age: 22 },
-      { id: '004', name: 'lmq', age: 21 }
-    ],
-    counter: 0
-  },
-  handleBtnClick(){
-    // console.log('按钮发生了点击')
-    // 1.错误做法
-    // this.data.counter += 1
-    // console.log(this.data.counter += 1)
+  // 3.监听wxml中相关的事件
+    handleGetUserInfo(event){
+      console.log(event)
+
+      //点击打印我的信息
+      console.log(app.globalData.name),
+      console.log(app.globalData.age)
+
+    },
+    handleViewMyname(){
+      console.log('是个帅哥!!!')
+    },
+    // 2.初始化数据
+    data:{
+      message:"陆明清"  ,
+      list:[
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+        {id:'001',waihao:'lumingqing'},
+      
+      ]
+    },
+
+    //1.监听页面的生命周期
+
+    //页面被加载出来时
+    onLoad(){
+      console.log('onLoad	function 生命周期回调—监听页面加载')
+      //发送网络请求
+      // wx.requeset() {
+      //   url:'http://123.207.32.32.8000/recommend' ; 
+      // }  
 
 
-    // 2.this.setData()
-    this.setData({
-      // counter: 100
-      counter:this.data.counter +1
-    })
-  },
-  handleBtnClick() {
-    // console.log('按钮发生了点击')
-    // 1.错误做法
-    // this.data.counter += 1
-    // console.log(this.data.counter += 1)
+    },
+    //页面显示出来时
+    onShow(){
+        console.log('onShow	function 生命周期回调—监听页面显示')
+    },
+    //第一次渲染  如果关闭了 再打开不会触发
+    onReady(){
+      console.log('onReady	function 生命周期回调—监听页面初次渲染完成')
+    },
+    //页面被隐藏时
+    onHide(){
+      console.log('onHide	function	生命周期回调—监听页面隐藏')
+    },
+    //从另一个页面回到首页 那个页面被销毁时
+    onUnload(){
+      console.log('onUnload	function			生命周期回调—监听页面卸载')
+    },
+    // 4.其他事件
+    // 监听页面的滚动
+    onPageScroll(obj){
+      console.log(obj)
+    },
+    onReachBottom(){
+      console.log('页面滚到底部了')
+    },
+    onPullDownRefresh(){
+      console.log('页面被下拉')
+    }
 
 
-    // 2.this.setData()
-    this.setData({
-      // counter: 100
-      counter: this.data.counter + 1
-    })
-  },
-  handleSubtraction() {
-    this.setData({
-      counter: this.data.counter - 1
-    })
-  },
-  changeMeHobby(){
-    this.setData({
-      msg: "My favorite sport is badminton "
-    })
-  }
 }) 
