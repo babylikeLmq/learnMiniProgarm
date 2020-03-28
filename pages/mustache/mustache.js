a6ae9cbdd7df26ab91e61bba78197ad1f7e79505
lumingqing <1335881694@qq.com>
2020-03-28 14:33:30 +0800

    条件判断_列表渲染_wxml导入

--- pages/mustache/mustache.js
+++ pages/mustache/mustache.js
@@ -10,7 +10,15 @@ Page({
     firstName:'DJ',
     lastName:'mingqi',
     nowTime:new Date().toLocaleString(),
-    isShow:false
+    isActive:false,
+    isShow:false,
+    score:75,
+    movies:['恐怖游轮','小丑回魂','电锯惊魂','禁闭岛','饥饿站台'],
+    array:[
+      [1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]
+    ],
+    letters:'abcdefg'
+
   },
   // 小程序注册完成后，加载页面，触发onLoad方法。
   onLoad(){
@@ -22,7 +30,17 @@ Page({
   },
   handleChaneColor(){
     this.setData({
-      isShow : !this.data.isShow
+      isActive : !this.data.isShow
+    })
+  },
+  handleSwitchFontShow(){
+    this.setData({
+      isShow:!this.data.isShow
+    })
+  },
+  handleIncrement(){
+    this.setData({
+      score:this.data.score+5
     })
   }
 })
