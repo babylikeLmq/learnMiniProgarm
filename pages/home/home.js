13b8e1ab80fe5ab13b0c7bdaa16e1a9a8bd1e879
lumingqing <1335881694@qq.com>
2020-03-29 15:42:04 +0800

    事件

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,62 @@ Page({
    * 页面的初始数据
    */
   data: {
+    titles:['潮流','娱乐','篮球']
+  },
+  handleTouchStart(){
+    console.log('handleTouchStart')
+  },
+  handleTouchMove(){
+    console.log('handleTouchMove')
+  },
+  handleTouchEnd(){
+    console.log('handleTouchEnd')
+  },
+  handleTap(){
+    console.log('handleTap')
+  },
+  handleLongpress(){
+    console.log('handleLongpress')
+  },
+  handleEventClick(event){
+    console.log('handleEventClick',event)
+  },
+  handleEventEnd(event){
+    console.log('handleEventEnd',event)
+  },
+  handleOuter(event){
+    console.log('outer',event)
+  },
+  handleInner(event){
+    console.log('inner',event)
+  },
+  handleItemClickClick(event){
+    // console.log(event)
+    const dataset = event.currentTarget.dataset
+    const index = dataset.diyindex
+    const title = dataset.diyitem
+    console.log(title,index)
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 事件冒泡和捕获
+  handleCaptureView1(){
+    console.log('外层：handleCaptureView1')
+  },
+  handleCaptureView2(){
+    console.log('中间层：handleCaptureView2')
+  },
+  handleCaptureView3(){
+    console.log('里层：handleCaptureView3')
+  },
+  handleTap1(){
+    console.log('外层：handleTap1')
+  },
+  handleTap2(){
+    console.log('中间层：handleTap2')
+  },
+  handleTap3(){
+    console.log('里层：handleTap3')
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
